112572124157ed2e3d9e334592981b990bf74b4b
EdgarGoPr <egdeprada@hotmail.com>
2023-09-28 17:43:36 -0300

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={styles.html}>
       <body className={inter.className}>{children}</body>
     </html>
   )
