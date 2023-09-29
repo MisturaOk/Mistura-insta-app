29ff09603b4cca07af5f7dbf0e29fb0408ba1076
EdgarGoPr <egdeprada@hotmail.com>
2023-09-29 13:33:13 -0300

    connected buttons

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,19 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
+  const handleClick1 = () => {
+    window.location.href = 'https://open.spotify.com/intl-es/artist/5G92cL2m5BRKzz5GBYvaXP?si=AYIoxmgtShqAkNHaMEy71A'
+    console.log('me fui')
+  }
+  const handleClick2 = () => {
+    window.location.href = 'https://music.apple.com/us/artist/mistura/1586405987'
+    console.log('me fui')
+  }
+  
+  const handleClick3 = () => {
+    window.location.href = 'https://music.youtube.com/channel/UCIfveHGDVQxyX14ScxBMLng'
+    console.log('me fui')
+  }
   return (
     <main className={styles.container}>
       <div className={styles.imageContainer}>
@@ -14,14 +27,14 @@ export default function Home() {
       <div className={styles.buttonContainer}>
         <div className={`${styles.buttonGroup} ${styles.row}`}>
           <div>
-            <button className={styles.round1}></button>
-            <button className={styles.round2}></button>
-            <button className={styles.round3}></button>
+            <button className={styles.round1} onClick={handleClick1}></button>
+            <button className={styles.round2} onClick={handleClick2}></button>
+            <button className={styles.round3} onClick={handleClick3}></button>
           </div>
           <div>
-            <button className={styles.square1}></button>
-            <button className={styles.square2}></button>
-            <button className={styles.square3}></button>
+            <button className={styles.square1} onClick={handleClick1}></button>
+            <button className={styles.square2} onClick={handleClick2}></button>
+            <button className={styles.square3} onClick={handleClick3}></button>
           </div>
           <div>
           </div>
@@ -30,6 +43,3 @@ export default function Home() {
     </main>
   );
 }
-
-
-
