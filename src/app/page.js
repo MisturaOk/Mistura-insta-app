807425909336cd15241563cccd48ea49227aaf2a
EdgarGoPr <egdeprada@hotmail.com>
2023-10-10 23:18:53 -0300

    3 version

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
+import Head from 'next/head';
 import styles from './page.module.css';
 
 export default function Home() {
@@ -16,31 +17,36 @@ export default function Home() {
     window.location.href = 'https://music.youtube.com/channel/UCIfveHGDVQxyX14ScxBMLng'
   }
   return (
-    <main className={styles.container}>
-      <div className={styles.imageContainer}>
-        <div className={styles.image} />
-      </div>
-      <div className={styles.bar} />
-      <div className={styles.buttonContainer}>
-        <div className={`${styles.buttonGroup} ${styles.row}`}>
-          <div className={styles.squareButtons}>
-            <button className={styles.square1} onClick={handleClick1}>
-              <Image className={styles.spotiIMG} src='/spoti-logo.png' alt="" width={55} height={55} />
-              Spotify
-            </button>
-            <button className={styles.square2} onClick={handleClick2}>
-            <Image className={styles.appleIMG} src='/manzana-apple.png' alt="" width={55} height={55} />
-              Apple Music
-            </button>
-            <button className={styles.square3} onClick={handleClick3}>
-            <Image className={styles.ytIMG} src='/youtube-logo.png' alt="" width={55} height={55} />
-              Youtube Music
-            </button>
-          </div>
-          <div>
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.imageContainer}>
+          <div className={styles.image} />
+        </div>
+        <div className={styles.bar} />
+        <div className={styles.buttonContainer}>
+          <div className={`${styles.buttonGroup} ${styles.row}`}>
+            <div className={styles.squareButtons}>
+              <button className={styles.square} onClick={handleClick1}>
+                <Image className={styles.spotiIMG} src='/spoti-logo.png' alt="" width={55} height={55} />
+                Spotify
+              </button>
+              <button className={styles.square} onClick={handleClick2}>
+                <Image className={styles.appleIMG} src='/manzana-apple.png' alt="" width={55} height={55} />
+                Apple Music
+              </button>
+              <button className={styles.square} onClick={handleClick3}>
+                <Image className={styles.ytIMG} src='/youtube-logo.png' alt="" width={55} height={55} />
+                Youtube Music
+              </button>
+            </div>
+            <div>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
